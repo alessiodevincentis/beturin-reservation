f79fe6baff9e50533fae79472cd60a6386b91a77
Alessio De Vincentis <Alessio.DeVincentis@nttdata.com>
2021-12-30 15:28:11 +0100

    firebaseConfig environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyBOzmqjAa-X4zPpwhZdOnJYLkhqrRnXjtg",
-    authDomain: "beturin-bed-and-breakfast.firebaseapp.com",
+    authDomain: "reservation-beturin.firebaseapp.com",
     projectId: "beturin-bed-and-breakfast",
-    storageBucket: "beturin-bed-and-breakfast.appspot.com",
+    storageBucket: "reservation-beturin.appspot.com",
     messagingSenderId: "298670437380",
     appId: "1:298670437380:web:10c6905890a1fe8fee7151",
     measurementId: "G-NZPFWYTVSJ"
